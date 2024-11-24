747b03a96c64a2c34ce6f5f1e07b3cf43626bf0d
margoglvz <msgalvez06@gmail.com>
2024-11-24 00:41:16 -0800

    Welcome!

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/caretech_logo.png" alt="Caretech Logo" />
+        <p>Welcome to CareTech!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
