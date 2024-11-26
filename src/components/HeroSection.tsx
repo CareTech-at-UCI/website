e541ffa7d845183d6319590111991c592ca98068
margoglvz <msgalvez06@gmail.com>
2024-11-26 06:57:16 -0800

    Fixed part of font alignment

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,18 +2,24 @@ import React from 'react';
 
 const HeroSection: React.FC = () => {
   return (
-    <section className="flex flex-col items-center text-left bg-gray-50 p-12">
-        <div className="flex flex-nowrap space-x-4"> 
-            <h1 className="font-semibold text-5xl">Empowering early disease detection with innovative </h1>
-            <h1 className="font-jersey text-5xl mr-4">tech </h1>
-            <h1 className="font-semibold text-5xl">solutions</h1>
-        </div>     
-        <p className="text-lg max-w-2xl">
-            Student-led Organization at UCI dedicated to creating innovative healthcare apps and tech solutions that can transform the way we approach health.
-        </p>
-        <button className="mt-6 px-8 py-3 bg-blue-500 text-white rounded-md hover:bg-blue-600">
-            Get Involved
-        </button>
+    <section className="flex flex-col items-start text-left bg-gray-50 p-12">
+        <div className="max-w-screen-lg">
+          <div className="flex items-center space-x-4">
+            <div className="w-12 border-t border-black" />
+            <h2 className="font-semibold text-2xl">CareTech At UCI.</h2>
+          </div>
+          <div className="space-x-4 mt-3"> 
+              <h1 className="font-semibold text-8xl">Empowering early disease detection with innovative </h1>
+              <h1 className="font-jersey text-8xl mr-4">tech </h1>
+              <h1 className="font-semibold text-8xl mb-4">solutions</h1>
+          </div>     
+          <p className="font-normal max-w-2xl">
+              Student-led Organization at UCI dedicated to creating innovative healthcare apps and tech solutions that can transform the way we approach health.
+          </p>
+          <button className="mt-6 px-8 py-3 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+              Get Involved
+          </button>
+        </div>
     </section>
   );
 };
