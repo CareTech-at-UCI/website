a9ca7c4cd776f91145918bb00c6517ea5ae2c835
Priyansh Shah <Priyanshpokemon@gmail.com>
2024-11-29 22:40:06 -0800

    feat: adding our team

---
+++ src/components/Team.tsx
@@ -0,0 +1,158 @@
+import React from "react";
+
+interface TeamMember {
+  image: string;
+  firstName: string;
+  lastName: string;
+  title: string;
+  pronouns: string;
+}
+
+const Team = () => {
+  const teamMembers: TeamMember[] = [
+    {
+      image: "/images/pronsh.png",
+      firstName: "Pronk",
+      lastName: "SwagMaster",
+      title: "President",
+      pronouns: "he/him",
+    },
+    {
+      image: "/images/pronsh.png",
+      firstName: "Pronk",
+      lastName: "SwagMaster",
+      title: "President",
+      pronouns: "he/him",
+    },
+    {
+      image: "/images/pronsh.png",
+      firstName: "Pronk",
+      lastName: "SwagMaster",
+      title: "President",
+      pronouns: "he/him",
+    },
+    {
+      image: "/images/pronsh.png",
+      firstName: "Pronk",
+      lastName: "SwagMaster",
+      title: "President",
+      pronouns: "he/him",
+    },
+    {
+      image: "/images/pronsh.png",
+      firstName: "Pronk",
+      lastName: "SwagMaster",
+      title: "President",
+      pronouns: "he/him",
+    },
+    {
+      image: "/images/pronsh.png",
+      firstName: "Pronk",
+      lastName: "SwagMaster",
+      title: "President",
+      pronouns: "he/him",
+    },
+    {
+      image: "/images/pronsh.png",
+      firstName: "Pronk",
+      lastName: "SwagMaster",
+      title: "President",
+      pronouns: "he/him",
+    },
+    {
+      image: "/images/pronsh.png",
+      firstName: "Pronk",
+      lastName: "SwagMaster",
+      title: "President",
+      pronouns: "he/him",
+    },
+    {
+      image: "/images/pronsh.png",
+      firstName: "Pronk",
+      lastName: "SwagMaster",
+      title: "President",
+      pronouns: "he/him",
+    },
+    {
+      image: "/images/pronsh.png",
+      firstName: "Pronk",
+      lastName: "SwagMaster",
+      title: "President",
+      pronouns: "he/him",
+    },
+    {
+      image: "/images/pronsh.png",
+      firstName: "Pronk",
+      lastName: "SwagMaster",
+      title: "President",
+      pronouns: "he/him",
+    },
+    {
+      image: "/images/pronsh.png",
+      firstName: "Pronk",
+      lastName: "SwagMaster",
+      title: "President",
+      pronouns: "he/him",
+    },
+    {
+      image: "/images/pronsh.png",
+      firstName: "Pronk",
+      lastName: "SwagMaster",
+      title: "President",
+      pronouns: "he/him",
+    },
+  ];
+
+  const itemsPerRow = 5;
+  return (
+    <div className="min-h-screen w-full p-8">
+      <h1 className="text-8xl mt-16 text-center mb-8 font-jersey">
+        Our Team
+      </h1>
+      <div className="mx-4 px-4">
+        <div className="grid grid-rows-auto gap-y-16 gap-x">
+          {Array.from(Array(Math.ceil(teamMembers.length / itemsPerRow))).map(
+            (_, rowIndex) => (
+              <div
+                key={rowIndex}
+                className="flex flex-row flex-wrap justify-center gap-8"
+              >
+                {teamMembers
+                  .slice(rowIndex * itemsPerRow, (rowIndex + 1) * itemsPerRow)
+                  .map((member, index) => (
+                    <TeamCard key={index} member={member} />
+                  ))}
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const TeamCard = ({ member }: { member: TeamMember }) => {
+  return (
+    <div className="flex flex-col items-center p-4">
+      <div className="w-60 h-60 mb-4">
+        <img
+          src={member.image}
+          alt={`${member.firstName} ${member.lastName}`}
+          width={240}
+          height={240}
+          className="object-cover"
+        />
+      </div>
+      <h2 className="text-2xl font-bold text-center">
+        {member.firstName}
+      </h2>
+      <h2 className="text-2xl font-bold text-center mb-2">
+        {member.lastName}
+      </h2>
+      <p className="text-xl text-center mb-1">{member.title}</p>
+      <p className="text-md text-center">{member.pronouns}</p>
+    </div>
+  );
+};
+
+export default Team;
