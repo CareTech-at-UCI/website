50b4a1d8bce0c2395934ae3359a2433d8656550e
NoNathan17 <nathanong1217@gmail.com>
2024-12-01 22:39:29 -0800

    Adjusted padding

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Events: React.FC = () => {
   return (
-    <section className="bg-white font-jersey p-16 h-[60vh] flex justify-center">
+    <section className="bg-white font-jersey p-20 h-[60vh] flex justify-center">
        <div className="flex-col">
         <h2 className="text-6xl mb-4 text-center">Events</h2>
         <p className="font-normal text-2xl text-[#294B7B] mb-6">
