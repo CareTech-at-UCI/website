a6b31dc17b4059b3708b75fa9942fcb1487c23fa
NoNathan17 <nathanong1217@gmail.com>
2024-12-02 17:16:23 -0800

    Added semicolon

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 import AboutUs from '../components/AboutUs';
-import Events from '../components/Events'
+import Events from '../components/Events';
 import Footer from '../components/Footer';
 import DetectScroll from "../components/DetectScroll";
 import WhoWeAre from "../components/WhoWeAre";
