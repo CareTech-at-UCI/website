84315fdb46fec79761f47531564917dd71d09406
margoglvz <msgalvez06@gmail.com>
2024-12-18 02:13:17 -0800

    added alt and spacing

--- src/components/WhoWeAre.tsx
+++ src/components/WhoWeAre.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const WhoWeAre: React.FC = () => {
   return (
-    <section className="bg-white font-jersey p-20 mt-12 h-[80vh] flex justify-center">
+    <section className="bg-white font-jersey p-20 mt-12 h-[80vh] mb-11 flex justify-center">
         <div className="flex-col">
-            <h2 className="text-6xl mb-6 text-center">Who Is CareTech?</h2>
+            <h2 className="text-6xl mb-7 pb-7 text-center">Who Is CareTech?</h2>
             <div className="flex justify-between">
-                <p className="font-normal text-2xl text-[#294B7B] mb-6 mt-10 w-[42%]">
+                <p className="font-normal text-2xl text-[#294B7B] mt-10 w-[42%]">
                 We are a student-led organization that will collaborate with industry-leading 
                 companies to <b>bring groundbreaking healthcare inventions to life</b>, get <b>hands-on 
                 experience</b> working on side projects and <b>building healthcare tech innovations</b>, 
