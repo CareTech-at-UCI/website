410cf35e2442f9105894c236727ffb0fc2daff79
NoNathan17 <nathanong1217@gmail.com>
2024-12-30 16:26:25 -0800

    about and event section of home page also mobile friendly

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -3,33 +3,33 @@ import { Link } from 'react-router-dom';
 
 const AboutUs: React.FC = () => {
   return (
-    <section className="bg-gradient-to-r from-black to-[#294B7B] font-jersey p-12 h-[50vh]">
-      <div className="mx-auto flex md:flex-row items-center justify-between gap-8">
-        <div className="flex-col w-1/2">
-          <h2 className="text-6xl font-bold mb-4 text-white">About Us</h2>
-            <p className="font-normal text-2xl text-white">
+    <section className="bg-gradient-to-r from-black to-[#294B7B] font-jersey p-12 h-auto">
+      <div className="mx-auto grid-cols-1 md:flex md:grid-cols-2 items-center justify-items-center md:justify-between gap-16 h-auto">
+        <div className="text-center md:text-left justify-items-center md:justify-items-start flo-row md:flex-col md:w-1/2">
+          <h2 className="text-5xl md:text-6xl font-bold mb-4 text-white">About Us</h2>
+            <p className="font-normal text-sm md:text-2xl text-white">
             From developing apps that improve patient care to partnering with companies shaping the 
             future of healthcare, we’re here to make a real difference.
             </p>
           <div className="flex mt-6 gap-2">
             <Link
               to="/About"
-              className="font-normal text-2xl text-[#BEEEFF] flex items-center"
+              className="font-normal text-lg md:text-2xl text-[#BEEEFF] flex items-center"
             >
               More
               <img
-                className="w-fit h-fit ml-2"
+                className="size-4 md:w-fit md:h-fit ml-2"
                 src="/icons/lightblue_arrow.svg"
                 alt="LightBlueArrowIcon"
               />
             </Link>
           </div>
         </div>
-        <div className="flex w-1/2">
+        <div className="flex my-6 md:w-1/2">
             <img 
-            className="w-full"
-            src="/images/dna_bubbles.png"
-            alt="DNABubbleImg"></img>
+              className="w-full rounded-xl"
+              src="/images/dna_bubbles.png"
+              alt="DNABubbleImg"></img>
         </div>
       </div>
     </section>
--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Events: React.FC = () => {
   return (
-    <section className="bg-white font-jersey p-20 h-[60vh] flex justify-center">
+    <section className="bg-white font-jersey p-10 md:p-20 h-auto flex justify-center">
        <div className="flex-col">
-        <h2 className="text-6xl mb-4 text-center">Events</h2>
-        <p className="font-normal text-2xl text-[#294B7B] mb-6">
+        <h2 className="text-5xl md:text-6xl mb-2 md:mb-4 text-center">Events</h2>
+        <p className="font-normal text-sm md:text-2xl text-[#294B7B] mb-2 md:mb-6 text-center">
           We host events in [Location] at [time and day]!
         </p>
-        <div className="flex mt-6 justify-center">
-            <button
-            className="font-normal text-2xl text-[#0097FC]"
+        <div className="flex justify-center mb-32">
+            <Link
+              to="/Events"
+              className="font-normal text-lg md:text-2xl text-[#0097FC] flex items-center"
             >
-            View All Events
-            </button>
-            <img className='w-fit h-fit my-auto' src="/icons/blue_arrow.svg"></img>
+              View All Events
+              <img
+                className="size-10 md:w-fit md:h-fit"
+                src="/icons/blue_arrow.svg"
+                alt="BlueArrowIcon"
+              />
+            </Link>
         </div>
       </div>
     </section>
