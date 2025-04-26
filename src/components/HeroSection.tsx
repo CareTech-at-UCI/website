469ef3f540f7c8ed2bb2e53f305ef327118040a0
margoglvz <msgalvez06@gmail.com>
2025-04-25 18:02:04 -0700

    made home page more responsive

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ const HeroSection: React.FC = () => {
     className="bg-[url('/public/images/dna_strand_mobile.png')] md:bg-[url('/public/images/dnastrand_bkg.png')] 
               bg-contain bg-top md:bg-right bg-no-repeat h-auto flex flex-col items-start text-left p-6 md:p-14"
     >
-        <div className="sm:h-svh max-w-screen-lg pt-52 md:pt-20">
+        <div className="sm:h-svh max-w-screen-lg lg:pt-16 md:pt-40 sm:pt-72 pt-60">
           <div className="flex items-center justify-center md:justify-normal space-x-2 md:space-x-4 pt-12">
             <div className="w-5 md:w-12 border-t border-black" />
             <h2 className="font-semibold text-2xl md:text-3xl">CareTech at UCI.</h2>
@@ -23,10 +23,10 @@ const HeroSection: React.FC = () => {
           </p>
           
             {/* <a href="" target="_blank" rel="noopener noreferrer"> */}
-              <p className="flex mt-6 md:mt-11 px-6 md:px-4 py-3 md:py-3 bg-sky-blue italic font-medium text-lg md:text-lg rounded-sm justify-self-center md:justify-self-start">
-                Look out for applications in Fall Quarter
-                {/* <img className='w-5 md:w-7 h-fit my-auto pl-2 hover:animate-bounce' src="/icons/black_arrow.svg" alt="Arrow"></img> */}
-              </p>
+          <p className="flex mt-6 md:mt-11 px-6 md:px-4 py-3 md:py-3 bg-sky-blue italic font-medium text-lg md:text-lg rounded-sm justify-self-center md:justify-self-start">
+            Look out for applications in Fall Quarter
+            {/* <img className='w-5 md:w-7 h-fit my-auto pl-2 hover:animate-bounce' src="/icons/black_arrow.svg" alt="Arrow"></img> */}
+          </p>
             {/* </a> */}
           
         </div>
