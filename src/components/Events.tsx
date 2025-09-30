d3deee070d251aebe2ad983edb0f50184c5a9f16
margoglvz <msgalvez06@gmail.com>
2025-09-30 00:59:12 -0700

    updated room and time for this quarter

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -9,7 +9,7 @@ const Events: React.FC = () => {
           Events
         </h2>
         <p className="font-normal text-sm md:text-2xl text-[#294B7B] mb-2 md:mb-6 text-center">
-          We host events in DBH 3011 on Mondays 6:00-7:00PM!
+          We host events in [Room TBA] on Wednesday 4:00-5:00PM!
         </p>
         <div className="flex justify-center mb-32">
           <Link
